468401790405740791bc851df4ca72db554e3885
Alexander Dadukin <st235@yandex.ru>
2018-06-13 10:49:00 +0300

    add design

---
+++ app/src/main/java/github/com/st235/chiplayout/BitmapHelper.java
@@ -0,0 +1,53 @@
+package github.com.st235.chiplayout;
+
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.annotation.DrawableRes;
+import android.support.annotation.NonNull;
+
+public class BitmapHelper {
+
+    private BitmapHelper() {
+    }
+
+    public static Bitmap decodeSampledBitmapFromResource(Resources res,
+                                                         @DrawableRes int resId,
+                                                         int reqWidth, int reqHeight) {
+
+        // First decode with inJustDecodeBounds=true to check dimensions
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(res, resId, options);
+
+        // Calculate inSampleSize
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+
+        // Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeResource(res, resId, options);
+    }
+
+    public static int calculateInSampleSize(
+            @NonNull BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+
+            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+            // height and width larger than the requested height and width.
+            while ((halfHeight / inSampleSize) >= reqHeight
+                    && (halfWidth / inSampleSize) >= reqWidth) {
+                inSampleSize *= 2;
+            }
+        }
+
+        return inSampleSize;
+    }
+}
--- app/src/main/java/github/com/st235/chiplayout/MainActivity.java
+++ app/src/main/java/github/com/st235/chiplayout/MainActivity.java
@@ -1,7 +1,15 @@
 package github.com.st235.chiplayout;
 
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatImageView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.ViewTreeObserver;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        final AppCompatImageView feedImage = findViewById(R.id.insta_image);
+        feedImage.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                feedImage.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                feedImage.setImageBitmap(BitmapHelper.decodeSampledBitmapFromResource(getResources(),
+                        R.drawable.cat2, feedImage.getWidth(), feedImage.getHeight()));
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
     }
 }
